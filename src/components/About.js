c45a066989127f467b31aed47d2e668232add684
Naimhz <sekki691@gmail.com>
2023-12-23 02:45:56 +0100

    ajout skills

--- src/components/About.js
+++ src/components/About.js
@@ -1,7 +1,41 @@
 import React from 'react'
 
 const About = () => {
-  return <div>About</div>
+  return (
+    <div name="about" className="w-full h-screen bg-[#0a192f] text-gray-300">
+      <div className="flex flex-col justify-center items-center w-full h-full">
+        <div className="max-w-[1000px] w-full grid grid-cols-2 gap-8">
+          <div className="sm:text-right pb-8 pl-4">
+            <p className="text-4xl font-bold inline border-b-4 border-pink-600">
+              About
+            </p>
+          </div>
+          <div></div>
+        </div>
+        <div className="max-w-[1000px] w-full grid sm:grid-cols-2 gap-8 px-4">
+          <div className="sm:text-right text-4xl font-bold">
+            <p>
+              Développeur pasionné par l'informatique depuis l'enfance, chaque
+              projet est pour moi une nouvelle aventure.{' '}
+            </p>
+          </div>
+          <div>
+            <p>
+              Après avoir valider 1 an a l'IUT Informatique, j'ai décider de me
+              spécialiser dans le web et de me former au métier de développeur
+              fullstack. Je suis passionné par la création de logiciels de
+              qualité qui améliorent la vie de ceux qui m'entourent.
+            </p>
+            <p className="mt-3">
+              Vous cherchez un développeur passionné et polyvalent pour votre
+              prochain projet ? N'hésitez pas à me contacter pour discuter de
+              vos besoins.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default About
---
+++ src/components/Skills.js
@@ -0,0 +1,65 @@
+import React from 'react'
+
+import myskills from '../data/myskills.json'
+
+const Skills = () => {
+  return (
+    <div name="skills" className="w-full h-screen bg-[#0a192f] text-gray-300">
+      {/* Container */}
+      <div className="max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full">
+        <div>
+          <p className="text-4xl font-bold inline border-b-4 border-pink-600 ">
+            Skills
+          </p>
+          <p className="m-8 py-3 border-b-4">
+            Voici les technologies sur lesquels j'ai travaillé
+          </p>
+        </div>
+
+        <div className="w-full grid grid-cols-2 sm:grid-cols-4 gap-4 text-center py-8">
+          {myskills.map((skill) => (
+            <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500">
+              <img
+                className="w-20 mx-auto"
+                src={`/images/${skill.url}`}
+                alt={skill.name}
+              />
+              <p className="my-4">{skill.name}</p>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Skills
+/* <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500">
+            <img className="w-20 mx-auto" src={CSS} alt="HTML icon" />
+            <p className="my-4">CSS</p>
+          </div>
+          <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500">
+            <img className="w-20 mx-auto" src={JavaScript} alt="HTML icon" />
+            <p className="my-4">JAVASCRIPT</p>
+          </div>
+          <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500">
+            <img className="w-20 mx-auto" src={ReactImg} alt="HTML icon" />
+            <p className="my-4">REACT</p>
+          </div>
+          <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500">
+            <img className="w-20 mx-auto" src={GitHub} alt="HTML icon" />
+            <p className="my-4">GITHUB</p>
+          </div>
+          <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500">
+            <img className="w-20 mx-auto" src={Node} alt="HTML icon" />
+            <p className="my-4">NODE JS</p>
+          </div>
+          <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500">
+            <img className="w-20 mx-auto" src={Mongo} alt="HTML icon" />
+            <p className="my-4">MONGO DB</p>
+          </div>
+          <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500">
+            <img className="w-20 mx-auto" src={Tailwind} alt="HTML icon" />
+            <p className="my-4">TAILWIND</p>
+          </div>
+*/
