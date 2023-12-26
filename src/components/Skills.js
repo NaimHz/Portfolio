6ddbb38d8459951b12c081fbd48400466c0e5a5a
Naimhz <sekki691@gmail.com>
2023-12-26 19:00:40 +0100

    finalisation

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -8,7 +8,7 @@ const Skills = () => {
       <div className="max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full ">
         <div>
           <p className="text-4xl font-bold inline border-b-4 border-pink-600 ">
-            Skills
+            Compétences
           </p>
           <p className="m-8 py-3 border-b-4">
             Voici les technologies sur lesquels j'ai travaillé
