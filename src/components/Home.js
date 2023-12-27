9576613586191b38eb16eee4c865f4c74a2b333c
Naimhz <sekki691@gmail.com>
2023-12-27 16:45:10 +0100

    .

--- src/components/Home.js
+++ src/components/Home.js
@@ -18,7 +18,7 @@ const Home = () => {
           ligne.
         </p>
         <div>
-          <Link to="work" smooth={true} duration={500}>
+          <Link to="works" smooth={true} duration={500}>
             <button className="group text-white border-2 px-6 py-3 my-2 flex items-center hover:bg-pink-600 hover:border-pink-600">
               Voir projets{' '}
               <HiArrowNarrowRight className="ml-3 group-hover:rotate-90 duration-300" />
