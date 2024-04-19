bca510b0dfc463afc700993d79c2700acb2cc36e
Naimhz <sekki691@gmail.com>
2024-04-19 18:40:15 +0200

    orthographe home

--- src/components/Home.js
+++ src/components/Home.js
@@ -11,7 +11,7 @@ const Home = () => {
           Naïm Hamza-Zerigat
         </h1>
         <h2 className="text-4xl sm:text-7xl font-bold text-[#8892b0]">
-          Je suis dévelloppeur fullstack.
+          Je suis développeur fullstack.
         </h2>
         <p className="text-[#8892b0] py-4 max-w-[700px]">
           Je conçois et réalise des sites web du cahier des charges à la mise en
